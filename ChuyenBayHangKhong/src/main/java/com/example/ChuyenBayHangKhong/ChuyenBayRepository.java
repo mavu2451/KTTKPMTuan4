a22a6acf84a557dd3940483191c1a6bbde3caf12
mavu2451 <68577194+mavu2451@users.noreply.github.com>
2022-09-24 15:08:26 +0700

    Add files via upload

--- ChuyenBayHangKhong/src/main/java/com/example/ChuyenBayHangKhong/ChuyenBayRepository.java
+++ ChuyenBayHangKhong/src/main/java/com/example/ChuyenBayHangKhong/ChuyenBayRepository.java
@@ -1,6 +1,6 @@
 package com.example.ChuyenBayHangKhong;
 
-import com.example.ChuyenBayHangKhong.chuyenBay;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface ChuyenBayRepository extends CrudRepository<chuyenBay, Long> {
-    @Query(value = "SElECT * FROM chuyenbay where gaden = ?1",nativeQuery = true)
+    @Query(value = "SElECT * FROM chuyenbay where gaden = ?1", nativeQuery = true)
     public List<chuyenBay> getGaDen(String GaDen);
+    @Query(value = "SELECT * FROM chuyenbay where dodai < ?1 and dodai > ?2",nativeQuery = true)
+    public List<chuyenBay> getDoDai(Integer dodai1, Integer dodai2);
+    @Query(value = "SELECT * FROM chuyenbay where gadi = ?1 and gaden = ?2",nativeQuery = true)
+    public List<chuyenBay> getSGNBMV(String sg, String bmt);
 }
--- ChuyenBayHangKhong/src/main/java/com/example/ChuyenBayHangKhong/chuyenBayDAO.java
+++ ChuyenBayHangKhong/src/main/java/com/example/ChuyenBayHangKhong/chuyenBayDAO.java
@@ -9,20 +9,43 @@ import java.util.List;
 @Service
 public class chuyenBayDAO {
     @Autowired
-    private ChuyenBayRepository repository;
+    private ChuyenBayRepository chuyenBayRepository;
+    @Autowired
+    private MayBayRepository mayBayRepository;
+    @Autowired
+    private NhanVienRepository nhanVienRepository;
     public void getGaDen(){
-        List<chuyenBay> cb = repository.getGaDen("DAD");
+        List<chuyenBay> cb = chuyenBayRepository.getGaDen("DAD");
         System.out.println(cb);
     }
-    public void getAll(){
-        List<chuyenBay> cb = (List<chuyenBay>) repository.findAll();
+    public void getTamBay(){
+        List<mayBay> mb = mayBayRepository.getTamBay("10000");
+        System.out.println(mb);
+    }
+    public void getAllCB(){
+        List<chuyenBay> cb = (List<chuyenBay>) chuyenBayRepository.findAll();
+        System.out.println(cb);
+    }
+    public void getAllMB(){
+        List<mayBay> mb = (List<mayBay>) mayBayRepository.findAll();
+        System.out.println(mb);
+    }
+    public void getLuong(){
+        List<nhanVien> nv = nhanVienRepository.getLuong(10000);
+        System.out.println(nv);
+    }
+    public void getDoDaiChuyenBay(){
+        List<chuyenBay> cb = chuyenBayRepository.getDoDai(10000,8000);
+        System.out.println(cb);
+    }
+    public void getSGNBMV(){
+        List<chuyenBay> cb = chuyenBayRepository.getSGNBMV("SGN","BMV");
         System.out.println(cb);
     }
     @Bean
     public CommandLineRunner run(){
         return (args -> {
-            getGaDen();
-            System.out.println(repository.findAll());
+            getSGNBMV();
         });
     }
 }
